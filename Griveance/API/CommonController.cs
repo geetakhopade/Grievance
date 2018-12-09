29565b9f63b3277ad49d903e68de808a4021ebc1
geetakhopade <44231298+geetakhopade@users.noreply.github.com>
2018-12-09 15:58:16 +0530

    API Chnages-Bhagyashri
    Done All Assigned API

--- Griveance/API/CommonController.cs
+++ Griveance/API/CommonController.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Griveance.Models;
+using Griveance.BusinessLayer;
 
 namespace Griveance.Controllers
 {
     public class CommonController : ApiController
     {
+        public object ShowHomeWorkAll([FromBody]student_parameters objhome)
+        {
+             SetIsLiveForUser obj = new SetIsLiveForUser();
+            var homemworkresult = obj.UserLive(objhome);
+            return homemworkresult;
+        }
     }
 }
--- Griveance/API/MemberController.cs
+++ Griveance/API/MemberController.cs
@@ -4,10 +4,31 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Griveance.Models;
+using Griveance.BusinessLayer;
 
 namespace Griveance.Controllers
 {
     public class MemberController : ApiController
     {
+        [HttpPost]
+        public object MemberSave([FromBody]Member_parameter member_para)
+        {
+
+            MemberBusiness memberbus = new MemberBusiness();
+
+
+
+
+
+            var result = memberbus.SaveMember(member_para);
+            //  bhobj.StudentsMethod(hobj);
+
+            return result;
+
+
+
+
+        }
     }
 }
